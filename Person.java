44bd0b9c013b7943ecbdf0c5287884f8d501074e
fragoso.sean <fragoso.sean@student.ccm.edu>
2026-09-17 16:07:43 -0400

    Complete Week 2 Lab 2

--- Person.java
+++ Person.java
@@ -1,3 +1,45 @@
 public class Person {
-    
+    //Person
+
+    //String Name
+    //int Age
+    //String Email
+
+    //setName (String), getName()
+    //setAge (int), getAge()
+    //setEmail (String), getEmail()
+
+    //displayinfo()
+
+    private String Name;
+    private int Age;
+    private String Email;
+
+    public void setName(String n){
+        Name = n;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setAge(int a){
+        Age = a;
+    }
+
+    public int getAge(){
+        return Age;
+    }
+
+    public void setEmail(String e){
+        Email = e;
+    }
+
+    public String getEmail(){
+        return Email;
+    }
+
+    public void displayinfo(){
+        System.out.print("Name: "+getName() + "\t Age: " + getAge() + "\t Email: " + getEmail());
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class PersonTest {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        Person p1 = new Person();
+
+        String n, e;
+        int a;
+
+        System.out.print("Enter the name of the Person: ");
+        n = input.nextLine();
+        p1.setName(n);
+        System.out.print("Enter the age of the Person: ");
+        a = input.nextInt();
+        p1.setAge(a);
+        input.nextLine();
+        System.out.print("Enter the email of the Person: ");
+        e = input.nextLine();
+        p1.setEmail(e);
+
+        p1.displayinfo();
+    }
     
 }
